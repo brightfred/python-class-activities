53d9b150dcc09d89c6951e10b5e1f0153a7d04c5
Frederick.Laroche <139751013+brightfred@users.noreply.github.com>
2024-07-29 20:29:22 +1200

    fizzbuzz multiple of 3 and 5

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,11 +1,42 @@
-def fizz_buzz(up_to_number) :
-    for i in range(1, up_to_number + 1) :
-        if i % 3 == 0 and i % 5 == 0 :
-            answer = "FizzBuzz"
-        elif i % 3 == 0 :
-            answer = "Fizz"
-        elif i % 5 == 0 :
-            answer = "Buzz"
-        else :
-            answer = i
-        print(answer)
+
+def fizz_buzz(i,first,second):
+    if i % 3 == 0 and i % 5 == 0 :
+        answer = "FizzBuzz"
+    elif i % 3 == 0:
+        answer = "Fizz"
+     elif i % 5 == 0 :
+        answer = "Buzz"
+     else:
+        answer = str(i)
+        
+        print(i, answer)
+        
+
+
+
+def get_fizzbuzz(i):
+    if i % 3 == 0 and i % 5 == 0:
+       answer = "FizzBuzz"
+    elif i % 3 == 0:
+        answer = "Fizz"
+    elif i % 5 == 0:
+        answer = "Buzz"
+    else:
+        answer = str(i)
+    return answer
+
+
+def fizz_buzz_return_list(up_to_number):
+    result_list = []
+    answer = ""
+    for i in range(up_to_number):
+        answer = get_fizzbuzz(i)
+       
+        result_list  += [i,answer]
+
+
+def fizz_buzz_comprehension(up_to_number):
+    return [(i, get_fizzbuzz(i)) for i in range(up_to_number)]
+
+if __name__ == "__main__":
+    fizz_buzz(25)
--- use_fizz_buzz.py
+++ use_fizz_buzz.py
@@ -1,4 +1,4 @@
 import fizz_buzz as fb
 
-if __name__=="__main__":
+if __name__ == "__main__":
     fb.fizz_buzz(25)
